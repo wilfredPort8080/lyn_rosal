bb289f594f35f2ee8aec3540d0e8e22a6eb07c5e
wilfredport8080 <bong2015sa@gmail.com>
2026-07-07 09:55:53 +0800

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,17 +3,17 @@ import styles from "./About.module.css";
 
 const content = [
   {
-    summary: "About Me",
+    summary: "Research Director Profile",
     details:
       "Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
-    summary: "Profile",
+    summary: "Academic Excellence",
     details:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
-    summary: "Degree",
+    summary: "Doctoral Achievements",
     details:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
   },
